1e9ea014a969182372005d3012dbc2b419335242
Cole <cole@colex.dev>
2025-03-05 18:12:26 -0500

    I implemented font stuff but am not in lab to test

--- firmware/include/ui.h
+++ firmware/include/ui.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+#include "../include/font.h"
+
 #define FRONT (1)
 #define BACK  (0)
 #define MAX_FRONT_SIZE     (100)
@@ -28,14 +30,14 @@ void draw_main_menu(uint8_t curr_selected_deck, char deck_names[][MAX_NAME_SIZE]
 void draw_hline(uint16_t x, uint16_t y, uint16_t length, uint8_t col);
 void draw_vline(uint16_t x, uint16_t y, uint16_t length, uint8_t col);
 void draw_rect(uint16_t s_x, uint16_t s_y, uint16_t e_x, uint16_t e_y, uint8_t col);
-void draw_string(uint16_t s_x, uint16_t s_y, char* string, uint8_t col);
-void draw_char(uint16_t s_x, uint16_t s_y, uint16_t c, uint8_t col);
-void draw_centered_string_in_rect(uint16_t s_x, uint16_t s_y, uint16_t e_x, uint16_t e_y, char* string, uint8_t col);
-void draw_centered_string_wrapped(char* string, uint8_t col);
-void draw_centered_string_in_filled_rect(uint16_t s_x, uint16_t s_y, uint16_t e_x, uint16_t e_y, char* string, uint8_t col);
+void draw_string(struct font f, uint16_t s_x, uint16_t s_y, char* string, uint8_t col);
+void draw_char(struct font f, uint16_t s_x, uint16_t s_y, uint16_t c, uint8_t col);
+void draw_centered_string_in_rect(struct font f, uint16_t s_x, uint16_t s_y, uint16_t e_x, uint16_t e_y, char* string, uint8_t col);
+void draw_centered_string_wrapped(struct font f, char* string, uint8_t col);
+void draw_centered_string_in_filled_rect(struct font f, uint16_t s_x, uint16_t s_y, uint16_t e_x, uint16_t e_y, char* string, uint8_t col);
 void draw_filled_rect(uint16_t s_x, uint16_t s_y, uint16_t e_x, uint16_t e_y, uint8_t col);
 void draw_flashcard(struct flashcard fc, uint8_t f_b, uint8_t col);
-void draw_string_wrapped(uint16_t s_x, uint16_t s_y, char* string, uint8_t col);
+void draw_string_wrapped(struct font f, uint16_t s_x, uint16_t s_y, char* string, uint8_t col);
 void draw_header(char* title);
 
 #endif /* UI_H */
